4c7c6efac8e1c4399910d1c5efdcbbeee9d6e7e0
TrxMod21X <7teukurizky@gmail.com>
2023-07-30 15:36:08 +0700

    adding

---
+++ .config/bspwm/themes/current.bash
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------------------------
+# Copyright (C) 2020-2022 TrxMod21X <7teukurizky@gmail.com>
+# Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+#
+# TrxMod21X color scheme
+# -----------------------------------------------------------------------------
+
+# Wallpaper
+wallpaper='/usr/share/backgrounds/wall-03.webp'
+
+# Special
+foreground='#F1F1F1'
+background='#1A1B26'
+cursor='#5AF78E'
+
+bgtrans = #00000000
+white = #FFFFFF
+black = #000000
+
+# Colors
+color0='#f7768e'
+color1='#FF0677'
+color2='#583794'
+color3='#7aa2f7'
+color4='#0A9CF5'
+color5='#4DD0E1'
+color6='#00B19F'
+color7='#9ece6a'
+color8='#B9C244'
+color9='#e0af68'
+color10='#FBC02D'
+color11='#E57C46'
+color12='#AC8476'
+color13='#8C8C8C'
+color14='#6C77BB'
+color15='#6D8895'
+
+# Don't Delete This File
---
+++ .config/bspwm/themes/trxmod21x.bash
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------------------
+# Copyright (C) 2020-2022 TrxMod21X <7teukurizky@gmail.com>
+# Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+#
+# TrxMod21X color scheme
+# -----------------------------------------------------------------------------
+
+# Wallpaper
+wallpaper='/usr/share/backgrounds/trx-wall.png'
+
+# Special
+foreground='#F8F8F2'
+background='#282A36'
+cursor='#5AF78E'
+
+bgtrans = #00282A36
+white = #FFFFFF
+black = #000000
+
+# Colors
+color0='#3C3E4A'
+color1='#FF5555'
+color2='#50FA7B'
+color3='#F1FA8C'
+color4='#BD93F9'
+color5='#FF79C6'
+color6='#8BE9FD'
+color7='#BFBFBF'
+color8='#4D4D4D'
+color9='#FF6E67'
+color10='#5AF78E'
+color11='#F4F99D'
+color12='#CAA9FA'
+color13='#FF92D0'
+color14='#9AEDFE'
+color15='#E6E6E6'
